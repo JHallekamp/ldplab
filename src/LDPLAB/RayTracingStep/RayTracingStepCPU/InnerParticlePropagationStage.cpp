da412eabc6f9ac7ca452aecbb4f84326fb880d86
Jonas Hallekamp <jonas.hallekamp@uni-muenster.de>
2020-12-27 16:24:20 +0100

    Add innner particle propagation stage implementation

---
+++ src/LDPLAB/RayTracingStep/RayTracingStepCPU/InnerParticlePropagationStage.cpp
@@ -0,0 +1,276 @@
+#include "InnerParticlePropagationStage.hpp"
+
+#include "Data.hpp"
+#include "Context.hpp"
+#include "../../ExperimentalSetup/Particle.hpp"
+#include "../../ExperimentalSetup/ParticleMaterial.hpp"
+
+ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::
+    LinearIndexGradientRodeParticlePropagation(
+        std::shared_ptr<Context> context,
+        const double initial_step_size,
+        const double epsilon,
+        const double safety_factor)
+    :
+    initial_step_size{ initial_step_size },
+    epsilon{ epsilon },
+    safety_factor{ safety_factor },
+    m_context{ context }
+{
+}
+
+void ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::execute(
+    RayBuffer& rays,
+    IntersectionBuffer& intersection)
+{
+    // TODO: if(intersection.num_intersections == 0) ?
+    for (size_t i = 0; i < rays.size; i++)
+    {
+        if (rays.index_data[i] < 0 && 
+            rays.index_data[i] >= m_context->particles.size())
+            continue;
+        if (rays.ray_data[i].intensity <= 0)
+            continue;
+
+        rayPropagation(
+            rays.index_data[i], 
+            rays.ray_data[i], 
+            intersection.point[i], 
+            intersection.normal[i]);
+        intersection.num_intersections++;
+    }
+}
+
+void ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::
+    rayPropagation(
+        const size_t particle, 
+        Ray& ray, 
+        Vec3& inter_point,
+        Vec3& inter_normal)
+{
+    ParticleMaterialLinearOneDirectional* material =
+        (ParticleMaterialLinearOneDirectional*) m_context->particles[particle]
+        .material.get();
+
+    bool intersected = false;
+    Arg x{
+        ray.direction * material->indexOfRefraction(ray.origin),
+        ray.origin };
+    Arg x_new{};
+    double h = initial_step_size;
+    while (!intersected)
+    {
+        double error = rk45(material, x, h, x_new);
+        if (error <= epsilon)
+        {
+            if (isOutsideParticle(
+                m_context->rode_particle_geometry[particle], x_new.r))
+            {
+                intersected = true;
+                intersection(
+                    m_context->rode_particle_geometry[particle], 
+                    x, 
+                    inter_point, 
+                    inter_normal);
+                return;
+            }
+            else
+            {
+                x = x_new;
+                h = safety_factor * h * std::pow(epsilon / error, 0.2);
+            }
+        }
+        else
+        {
+            h = safety_factor * h * std::pow(epsilon / error, 0.25);
+            // TODO: LDPLAB_LOG_TRACE("Step discarded, error = %f, new_h = %f", error, h))
+        }
+    }
+}
+
+bool ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::
+    isOutsideParticle(const RodeParticle& geometry, const Vec3& r)
+{
+    if (r.x * r.x + r.y * r.y >
+        geometry.cylinder_radius * geometry.cylinder_radius)
+        return false;
+
+    if (r.z < geometry.origin_cap.z + geometry.sphere_radius && r.z < 0)
+    {
+        if (r.z > geometry.cylinder_length)
+        {
+            double norm_r2 = r.x * r.x + r.y * r.y;
+            double radius2 = geometry.sphere_radius * geometry.sphere_radius -
+                std::pow(r.z-geometry.origin_cap.z,2.0);
+            if (norm_r2 > radius2)
+                return false;
+            else
+                return true;
+        }
+        else if (r.z < geometry.origin_indentation.z + geometry.sphere_radius)
+        {
+            double norm_r2 = r.x * r.x + r.y * r.y;
+            double radius2 = geometry.sphere_radius * geometry.sphere_radius -
+                std::pow(r.z - geometry.origin_indentation.z, 2.0);
+            if (norm_r2 < radius2)
+                return false;
+            else
+                return true;
+        }
+        else
+            return true;
+    }
+    else
+        return false;
+}
+
+double ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::rk45(
+    const ParticleMaterialLinearOneDirectional* particle,
+    const Arg& x,
+    const double h,
+    Arg& x_new)
+{
+    Arg k[6] {};
+    Arg error{ {0,0,0}, {0,0,0} };
+    for (size_t i = 0; i < 6; ++i)
+    {
+        Arg x_step = x;
+        for (size_t j = 0; j < i; ++j)
+            x_step = x_step + k[j] * h * beta[(i+1) * 6 + j];
+        k[i] = eikonal(particle, x_step);
+      
+        error = error + k[i] * cerr[i];
+        x_new = x_new + k[i] * c_star[i];
+    }
+    x_new = x_new * h + x;
+    error = error * h;
+    return error.absoluteMax();
+
+    //k[0] = eikonal(particle, x);
+    //k[1] = eikonal(particle, x + k[0] * h * beta[1, 0]);
+    //k[2] = eikonal(particle, x + (k[0] * beta[2, 0] + k[1] * beta[2, 1]) * h);
+    //k[3] = eikonal(particle, x + (k[0] * beta[3, 0] + k[1] * beta[3, 1] + k[2] * beta[3, 2]) * h);
+    //k[4] = eikonal(particle, x + (k[0] * beta[4, 0] + k[1] * beta[4, 1] + k[2] * beta[4, 2] + k[3] * beta[4, 3]) * h);
+    //k[5] = eikonal(particle, x + (k[0] * beta[5, 0] + k[1] * beta[5, 1] + k[2] * beta[5, 2] + k[3] * beta[5, 3] + k[4] * beta[5, 4]) * h);
+    //Arg errorfield = (k[0] * cerr[0] + k[1] * cerr[1] + k[2] * cerr[2] + k[3] * cerr[3] + k[4] * cerr[4] + k[5] * cerr[5]) * h;
+}
+
+ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::Arg 
+    ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::eikonal(
+        const ParticleMaterialLinearOneDirectional* particle,
+        Arg& x )
+{
+    Arg f_of_x{ 
+        particle->direction * particle->gradient, 
+        x.w / particle->indexOfRefraction(x.r) };
+    return f_of_x;
+}
+
+bool ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::
+    cylinderIntersection(
+        const RodeParticle& geometry, 
+        const Ray& ray, 
+        Vec3& inter_point,
+        Vec3& inter_normal)
+{
+    double p =
+        (ray.origin.x * ray.direction.x + ray.origin.y * ray.direction.y) /
+        (ray.direction.x * ray.direction.x + ray.direction.y + ray.direction.y);
+    double q = (ray.origin.x * ray.origin.x + ray.origin.y * ray.origin.y) -
+        geometry.cylinder_radius;
+    double discriminant = p * p - q;
+
+    if (discriminant < 0.0)
+        return false;
+    double t = -p - std::sqrt(discriminant);
+    if (t < 0)
+        double t = -p + std::sqrt(discriminant);
+
+    inter_point = ray.origin + ray.direction * t;
+    if (inter_point.z <= geometry.cylinder_length &&
+        inter_point.z >= 0)
+    {
+        inter_normal = { -inter_point.x, -inter_point.y,0 };
+        inter_normal = glm::normalize(inter_normal);
+        return true;
+    }
+    return false;
+    
+}
+
+bool ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::
+    capIntersection(
+        const RodeParticle& geometry,
+        const Ray& ray, 
+        Vec3& inter_point, 
+        Vec3& inter_normal)
+{
+    Vec3 o_minus_c = ray.origin - geometry.origin_cap;
+    double p = glm::dot(ray.direction, o_minus_c);
+    double q = dot(o_minus_c, o_minus_c) -
+        (geometry.sphere_radius * geometry.sphere_radius);
+    double discriminant = (p * p) - q;
+
+    if (discriminant < 0.0)
+        return false;
+    double t = -p - std::sqrt(discriminant);
+    if (t >= 0)
+        t = -p + std::sqrt(discriminant);
+
+    inter_point = ray.origin + t * ray.direction;
+    if (inter_point.z > geometry.cylinder_length &&
+        inter_point.z <= geometry.origin_cap.z +
+        geometry.sphere_radius)
+    {
+        inter_normal = glm::normalize(
+            inter_point - geometry.origin_cap);
+        return true;
+    }
+    return false;
+}
+
+bool ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::
+indentationIntersection(
+    const RodeParticle& geometry,
+    const Ray& ray,
+    Vec3& inter_point,
+    Vec3& inter_normal)
+{
+    Vec3 o_minus_c = ray.origin - geometry.origin_indentation;
+    double p = glm::dot(ray.direction, o_minus_c);
+    double q = dot(o_minus_c, o_minus_c) - 
+        (geometry.sphere_radius * geometry.sphere_radius);
+    double discriminant = (p * p) - q;
+
+    if (discriminant < 0.0)
+        return false;
+    double t = -p - std::sqrt(discriminant);
+    if (t >= 0)
+        t = -p + std::sqrt(discriminant);
+
+    inter_point = ray.origin + t * ray.direction;
+    if (inter_point.z > 0 &&
+        inter_point.z <= geometry.origin_indentation.z +
+        geometry.sphere_radius)
+    {
+        inter_normal = glm::normalize(
+            inter_point - geometry.origin_indentation);
+        return true;
+    }
+    return false;
+}
+
+void ldplab::rtscpu::LinearIndexGradientRodeParticlePropagation::intersection(
+    const RodeParticle& geometry,
+    Arg& ray, 
+    Vec3& inter_point,
+    Vec3& inter_normal)
+{
+    Ray t_ray{ ray.r, glm::normalize(ray.w), -1};
+    if (indentationIntersection(geometry, t_ray, inter_point, inter_normal))
+        return;
+    if (cylinderIntersection(geometry, t_ray, inter_point, inter_normal))
+        return;
+    if (capIntersection(geometry, t_ray, inter_point, inter_normal))
+        return;
+}
--- src/LDPLAB/RayTracingStep/RayTracingStepCPU/InnerParticlePropagationStage.hpp
+++ src/LDPLAB/RayTracingStep/RayTracingStepCPU/InnerParticlePropagationStage.hpp
@@ -1,15 +1,23 @@
 #ifndef WWU_LDPLAP_RTSCPU_INNER_PARTICLE_PROPAGATION_STAGE_HPP
 #define WWU_LDPLAP_RTSCPU_INNER_PARTICLE_PROPAGATION_STAGE_HPP
 
+#include <cmath>
 #include <memory>
+#include "../../Geometry.hpp"
+
 
 namespace ldplab
 {
+    // Prototype
+    struct Particle;
+    struct ParticleMaterialLinearOneDirectional;
+
     namespace rtscpu
     {
         // Prototype
         struct Context;
         struct RayBuffer;
+        struct RodeParticle;
 
         /**
          * @brief Inner particle propagation stage interface
@@ -22,14 +30,159 @@ namespace ldplab
         public:
             /**
              * @brief Calculating the path of the rays threw the particle.
-             * @param[in] particle Index of the particle.
              * @param[in, out] rays RayBuffer holding the propagating rays. 
              */
             virtual void execute(
-                const size_t particle,
-                RayBuffer& rays) = 0;
+                RayBuffer& rays,
+                IntersectionBuffer& intersection) = 0;
         };
 
+        /**
+         * @brief Class implementing the inner particle propagation for 
+         *        linear index of refraction gradient in one direction.
+         * @detail The Light propagation is calculated by solving the Eikonal 
+         *         equation with the Runge–Kutta–Fehlberg method.
+         */
+        class LinearIndexGradientRodeParticlePropagation
+            : public IInnerParticlePropagationStage
+        {
+        public:
+            LinearIndexGradientRodeParticlePropagation(
+                std::shared_ptr<Context> context,
+                const double initial_step_size,
+                const double epsilon,
+                const double safety_factor);
+            /**
+             * @brief Inherited via ldplab::rtscpu::IInnerParticlePropagationStage.  
+             * @details Calculating the path of the rays threw the particle.
+             * @param[in, out] rays RayBuffer holding the propagating rays.
+             */
+            void execute(
+                RayBuffer& rays,
+                IntersectionBuffer& intersection) override;
+        private:
+            struct Arg
+            {
+                Vec3 w;
+                Vec3 r;
+                Arg operator*(const double& d) 
+                {
+                    Arg arg { this->w * d, this->r * d };
+                    return arg; 
+                }
+                Arg operator+(const Arg& a)
+                {
+                    Arg arg{ this->w + a.w, this->r + a.r };
+                    return arg;
+                }
+                double absoluteMax()
+                {
+                    double max = std::abs(w.x);
+                    return max;
+                }
+            };
+            void rayPropagation(
+                const size_t particle, 
+                Ray& ray, 
+                Vec3& inter_point,
+                Vec3& inter_normal);
+            bool isOutsideParticle(const RodeParticle& geometry, const Vec3& r);
+            double rk45(
+                const ParticleMaterialLinearOneDirectional* particle, 
+                const Arg& x,
+                const double h,
+                Arg& x_new);
+            Arg eikonal(
+                const ParticleMaterialLinearOneDirectional* particle, 
+                Arg& x);
+
+            void intersection(
+                const RodeParticle& geometry,
+                Arg& ray,
+                Vec3& inter_point,
+                Vec3& inter_normal);
+
+            /**
+             * @brief Calculate the intersection point of a ray and the cylinder.
+             * @warning It is assumed that the rays origin is inside the 
+             *          particle.
+             * @param[in] geometry Specifies the particle geometry.
+             * @param[in] ray Specifies the ray.
+             * @param[out] inter_point Resulting intersection point with
+             *                         the cap.
+             * @param[out] inter_normal Resulting normal of the particle
+             *                          surface at the intersection
+             *                          point. The normal is pointing
+             *                          inside the particle.
+             * @retuns true if the cylinder and the ray are intersecting, else
+             *         false will be returned.
+             */
+            bool cylinderIntersection(
+                const RodeParticle& geometry,
+                const Ray& ray,
+                Vec3& inter_point,
+                Vec3& inter_normal);
+            /**
+             * @brief Calculate the intersection point of a ray and the
+             *        spherical cap of the rode particle.
+             * @warning It is assumed that the rays origin is inside the 
+             *          particle.
+             * @param[in] geometry Specifies the particle geometry.
+             * @param[in] ray Specifies the ray.
+             * @param[out] inter_point Resulting intersection point with
+             *                         the cap.
+             * @param[out] inter_normal Resulting normal of the particle
+             *                          surface at the intersection
+             *                          point. The normal is pointing
+             *                          inside the particle.
+             * @retuns true if the cap and the ray are intersecting, else
+             *         false will be returned.
+             */
+            bool capIntersection(
+                const RodeParticle& geometry,
+                const Ray& ray,
+                Vec3& inter_point,
+                Vec3& inter_normal);
+            /**
+             * @brief Calculate the intersection point of a ray and the
+             *        spherical indentation of the rode particle.
+             * @warning It is assumed that the rays origin is inside the 
+             *          particle.
+             * @param[in] geometry Specifies the particle geometry.
+             * @param[in] ray Specifies the ray.
+             * @param[out] intersection_point Resulting intersection point with
+             *                                the indentation.
+             * @param[out] intersection_normal Resulting normal of the particle
+             *                                 surface at the intersection
+             *                                 point. The normal is pointing
+             *                                 inside the particle.
+             * @retuns true if the indentation and the ray are intersecting, 
+             *         else false will be returned.
+             */
+            bool indentationIntersection(
+                const RodeParticle& geometry,
+                const Ray& ray,
+                Vec3& inter_point,
+                Vec3& inter_normal);
+
+        private:
+            const double initial_step_size;
+            const double epsilon;
+            const double safety_factor;
+            const double alpha[6] { 0.0, 1.0/4.0, 3.0/8.0, 12.0/13.0, 1.0, 1.0/2.0 };
+            const double beta[36]{
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ,
+                1.0 / 4.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                3.0 / 32.0, 9.0 / 32.0, 0.0, 0.0, 0.0, 0.0,
+                1932.0 / 2197.0, (-7200.0) / 2197.0, 7296.0 / 2197.0, 0.0, 0.0, 0.0,
+                439.0 / 216.0, -8.0, 3680.0 / 513.0, (-845.0) / 4104.0, 0.0, 0.0,
+                (-8.0) / 27.0, 2.0, (-3544.0) / 2565.0, 1859.0 / 4104.0, (-11.0) / 40.0, 0.0 };
+            const double c[6] { 25.0 / 216.0, 0.0, 1408.0 / 2565.0, 2197.0 / 4104.0, (-1.0) / 5.0, 0.0 };
+            const double c_star[6] { 16.0 / 135.0, 0.0, 6656.0 / 12825.0, 28561.0 / 56430.0, (-9.0) / 50.0, 2.0 / 55.0 };
+            const double cerr[6] { -0.00277778,  0.0 ,  0.02994152,  0.02919989, -0.02 , -0.03636364 };
+        private:
+            std::shared_ptr<Context> m_context;
+        };
     }
 }
 
