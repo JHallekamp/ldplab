93d91e577c5286f3cbb5050d5c1495c391a6250b
Jonas Hallekamp <jonas.hallekamp@uni-muenster.de>
2020-12-28 18:42:37 +0100

    Add implementation of raytracingcpu factory

--- src/LDPLAB/RayTracingStep/RayTracingStepCPUInfo.hpp
+++ src/LDPLAB/RayTracingStep/RayTracingStepCPUInfo.hpp
@@ -16,6 +16,16 @@ namespace ldplab
     {
         std::shared_ptr<ExperimentalSetup> setup;
         std::shared_ptr<ThreadPool> thread_pool;
+        /**  @brief Under this cutoff intensity rays are not further traced. */
+        double intensity_cutoff;
+        /** @brief Number of rays per buffer. */
+        size_t number_rays_per_buffer;
+        /** @brief Number of rays per world space unit. */
+        size_t number_rays_per_unit;
+        /** @brief Maximum number of times a ray can split. */
+        size_t maximum_depth;
+        /** @brief Number of parallel pipeline instances. */
+        size_t number_parallel_pipelines;
     };
 }
 
---
+++ src/LDPLAB/RayTracingStep/RayTracingStepFactory.cpp
@@ -0,0 +1,56 @@
+#include "RayTracingStepFactory.hpp"
+
+#include "RayTracingStepCPU/Data.hpp"
+
+std::shared_ptr<ldplab::rtscpu::RayTracingStepCPU> ldplab::RayTracingStepFactory::
+    createRayTracingStepCPU(RayTracingStepCPUInfo& info)
+{
+
+    std::shared_ptr<rtscpu::Context> ctx{ new rtscpu::Context{} };
+    ctx->particles = info.setup->particles;
+    ctx->particle_transformations.resize(ctx->particles.size());
+    initGeometry(info, ctx);
+    ctx->light_sources = info.setup->light_sources;
+    ctx->intensity_cutoff = info.intensity_cutoff;
+    ctx->number_rays_per_buffer = info.number_rays_per_buffer;
+    ctx->number_rays_per_unit = info.number_rays_per_unit;
+    ctx->maximum_depth = info.maximum_depth;
+    ctx->number_parallel_pipelines = info.number_parallel_pipelines;
+
+    std::shared_ptr<rtscpu::RayTracingStepCPU> rts_cpu{ 
+        new rtscpu::RayTracingStepCPU{ctx}};
+
+    return rts_cpu;
+}
+
+void ldplab::RayTracingStepFactory::initGeometry(
+    RayTracingStepCPUInfo& info, std::shared_ptr<rtscpu::Context> context)
+{
+    for (size_t i = 0; i < info.setup->particles.size(); ++i)
+    {
+        Particle& particle = info.setup->particles[i];
+
+        if (particle.geometry->type() ==
+            IParticleGeometry::Type::sphere_capped_cylinder)
+        {
+            RodeParticleGeometry* geometry =
+                (RodeParticleGeometry*)particle.geometry.get();
+            double h = geometry->kappa * geometry->cylinder_radius;
+            double sphere_radius =
+                (h + geometry->cylinder_radius * geometry->cylinder_radius / h) / 2.0;
+            Vec3 origin_cap{ 0,0, geometry->cylinder_hight + h - sphere_radius };
+            Vec3 origin_indentation{ 0,0,h - sphere_radius };
+            context->rode_particle_geometry.emplace_back(
+                geometry->cylinder_radius,
+                geometry->cylinder_hight,
+                sphere_radius,
+                origin_cap,
+                origin_indentation);
+        }
+        else
+        {
+            // TODO: ERROR
+        }
+    }
+}
+
