cac37bf1259b45261ae1ba35b040c512e28cd13a
Jonas Hallekamp <jonas.hallekamp@uni-muenster.de>
2021-02-08 13:19:45 +0100

    Fix center of math formula

--- src/LDPLAB/ExperimentalSetup/Utils/PropertyGenerator.cpp
+++ src/LDPLAB/ExperimentalSetup/Utils/PropertyGenerator.cpp
@@ -8,11 +8,8 @@ ldplab::Vec3 ldplab::getRodParticleCenterOfMass(
     const ldplab::RodParticleGeometry& geometry)
 {
     Vec3 rs(0, 0, 0);
-    rs.z = -(geometry.cylinder_length / 2) +
-        (geometry.cylinder_length * geometry.cylinder_length * geometry.kappa) /
-        geometry.cylinder_radius +
+    rs.z = geometry.cylinder_length / 2 +
         (geometry.cylinder_radius * geometry.kappa) / 2 +
-        (geometry.cylinder_length * geometry.kappa * geometry.kappa) / 2 -
         (geometry.cylinder_radius * geometry.kappa * geometry.kappa *
             geometry.kappa) / 6;
     return rs;
