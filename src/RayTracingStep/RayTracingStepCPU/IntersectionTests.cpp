f9a69d7df80ab42d9ff90716510cc97397fbcc95
Alexander Gerwing <a_gerw03@uni-muenster.de>
2021-03-30 20:23:58 +0200

    Add helper class for intersection tests

---
+++ src/RayTracingStep/RayTracingStepCPU/IntersectionTests.cpp
@@ -0,0 +1,188 @@
+#include "IntersectionTests.hpp"
+
+inline bool ldplab::rtscpu::IntersectionTest::rayTriangle(
+    const Ray& ray, 
+    const Triangle& triangle, 
+    Vec3& intersection_point, 
+    Vec3& intersection_normal, 
+    double& dist)
+{
+    const double EPSILON = 1e-10;
+    const Vec3 edge1 = triangle.b - triangle.a;
+    const Vec3 edge2 = triangle.c - triangle.a;
+    const Vec3 h = glm::cross(ray.direction, edge2);
+    const double a = glm::dot(edge1, h);
+    // Check parallelisim
+    if (a > -EPSILON && a < EPSILON)
+        return false;
+    const double f = 1.0 / a;
+    const Vec3 s = ray.origin - triangle.a;
+    const double u = f * glm::dot(s, h);
+    if (u < 0.0 || u > 1.0)
+        return false;
+    const Vec3 q = glm::cross(s, edge1);
+    const double v = f * glm::dot(ray.direction, q);
+    if (v < 0.0 || u + v > 1.0)
+        return false;
+    // Calculate distance to intersection point
+    double t = f * glm::dot(edge2, q);
+    if (t > EPSILON)
+    {
+        dist = t;
+        intersection_point = ray.origin + ray.direction * t;
+        intersection_normal = glm::normalize(glm::cross(edge1, edge2));
+        if (glm::dot(intersection_normal, ray.direction) > 0)
+            intersection_normal = -intersection_normal;
+        return true;
+    }
+    else
+        return false;
+}
+
+inline bool ldplab::rtscpu::IntersectionTest::lineTriangle(
+    const Vec3& line_start, 
+    const Vec3& line_end, 
+    const Triangle& triangle, 
+    Vec3& intersection_point, 
+    Vec3& intersection_normal, 
+    double& dist)
+{
+    const double EPSILON = 1e-10;
+    const Vec3 line_direction = glm::normalize(line_end - line_start);
+    const Vec3 edge1 = triangle.b - triangle.a;
+    const Vec3 edge2 = triangle.c - triangle.a;
+    const Vec3 h = glm::cross(line_direction, edge2);
+    const double a = glm::dot(edge1, h);
+    // Check parallelisim
+    if (a > -EPSILON && a < EPSILON)
+        return false;
+    const double f = 1.0 / a;
+    const Vec3 s = line_start - triangle.a;
+    const double u = f * glm::dot(s, h);
+    if (u < 0.0 || u > 1.0)
+        return false;
+    const Vec3 q = glm::cross(s, edge1);
+    const double v = f * glm::dot(line_start, q);
+    if (v < 0.0 || u + v > 1.0)
+        return false;
+    // Calculate distance to intersection point
+    double t = f * glm::dot(edge2, q);
+    if (t > EPSILON && t <= glm::length(line_end - line_start))
+    {
+        dist = t;
+        intersection_point = line_start + line_direction * t;
+        intersection_normal = glm::normalize(glm::cross(edge1, edge2));
+        if (glm::dot(intersection_normal, line_direction) > 0)
+            intersection_normal = -intersection_normal;
+        return true;
+    }
+    else
+        return false;
+}
+
+inline bool ldplab::rtscpu::IntersectionTest::raySphere(
+    const Ray& ray, 
+    const Vec3& sphere_center, 
+    const double sphere_radius, 
+    double& min_dist, 
+    double& max_dist)
+{
+    const Vec3 o_minus_c = ray.origin - sphere_center;
+
+    const double p = glm::dot(ray.direction, o_minus_c);
+    const double q = glm::dot(o_minus_c, o_minus_c) -
+        (sphere_radius * sphere_radius);
+
+    const double discriminant = (p * p) - q;
+    if (discriminant < 1e-9)
+        return false;
+
+    min_dist = -p - std::sqrt(discriminant);
+    max_dist = -p + std::sqrt(discriminant);
+    return true;
+}
+
+inline bool ldplab::rtscpu::IntersectionTest::raySphere(
+    const Ray& ray, 
+    const Vec3& sphere_center, 
+    const double sphere_radius, 
+    Vec3& min_intersection_point, 
+    Vec3& min_intersection_normal,
+    double& min_dist,
+    Vec3& max_intersection_point, 
+    Vec3& max_intersection_normal, 
+    double& max_dist)
+{
+    const Vec3 o_minus_c = ray.origin - sphere_center;
+
+    const double p = glm::dot(ray.direction, o_minus_c);
+    const double q = glm::dot(o_minus_c, o_minus_c) - 
+        (sphere_radius * sphere_radius);
+
+    const double discriminant = (p * p) - q;
+    if (discriminant < 1e-9)
+        return false;
+    
+    // Calculate intersections
+    min_dist = -p - std::sqrt(discriminant);
+    min_intersection_point = ray.origin + min_dist * ray.direction;
+    min_intersection_normal = 
+        glm::normalize(min_intersection_point - sphere_center);
+    max_dist = -p + std::sqrt(discriminant);
+    max_intersection_point = ray.origin + max_dist * ray.direction;
+    max_intersection_normal =
+        glm::normalize(max_intersection_point - sphere_center);
+    return true;
+}
+
+inline bool ldplab::rtscpu::IntersectionTest::raySphereOnlyMin(
+    const Ray& ray, 
+    const Vec3& sphere_center, 
+    const double sphere_radius, 
+    Vec3& intersection_point, 
+    Vec3& intersection_normal, 
+    double& dist)
+{
+    const Vec3 o_minus_c = ray.origin - sphere_center;
+
+    const double p = glm::dot(ray.direction, o_minus_c);
+    const double q = glm::dot(o_minus_c, o_minus_c) -
+        (sphere_radius * sphere_radius);
+
+    const double discriminant = (p * p) - q;
+    if (discriminant < 1e-9)
+        return false;
+
+    // Calculate intersections
+    dist = -p - std::sqrt(discriminant);
+    intersection_point = ray.origin + dist * ray.direction;
+    intersection_normal =
+        glm::normalize(intersection_point - sphere_center);
+    return true;
+}
+
+inline bool ldplab::rtscpu::IntersectionTest::raySphereOnlyMax(
+    const Ray& ray, 
+    const Vec3& sphere_center, 
+    const double sphere_radius, 
+    Vec3& intersection_point, 
+    Vec3& intersection_normal, 
+    double& dist)
+{
+    const Vec3 o_minus_c = ray.origin - sphere_center;
+
+    const double p = glm::dot(ray.direction, o_minus_c);
+    const double q = glm::dot(o_minus_c, o_minus_c) -
+        (sphere_radius * sphere_radius);
+
+    const double discriminant = (p * p) - q;
+    if (discriminant < 1e-9)
+        return false;
+
+    // Calculate intersections
+    dist = -p + std::sqrt(discriminant);
+    intersection_point = ray.origin + dist * ray.direction;
+    intersection_normal =
+        glm::normalize(intersection_point - sphere_center);
+    return true;
+}
---
+++ src/RayTracingStep/RayTracingStepCPU/IntersectionTests.hpp
@@ -0,0 +1,83 @@
+#ifndef WWU_LDPLAB_RTSCPU_INTERSECTION_TESTS_HPP
+#define WWU_LDPLAB_RTSCPU_INTERSECTION_TESTS_HPP
+
+#include <LDPLAB/Geometry.hpp>
+
+namespace ldplab
+{
+    namespace rtscpu
+    {
+        /** @brief Contains static methods for various intersection tests. */
+        class IntersectionTest
+        {
+            /** @brief No instances of this class allowed. */
+            IntersectionTest() = delete;
+            /** 
+             * @brief Computes the intersection between a ray and a triangle.
+             * @param[in] ray Ray for which the test is executed.
+             * @param[in] triangle Triangle for which the test is executed.
+             * @param[out] intersection_point If ray and triangle intersect,
+             *                                intersection_point will contain
+             *                                the point of intersection.
+             * @param[out] intersection_normal If ray and triangle intersect,
+             *                                 intersection_normal will contain
+             *                                 the triangle normal at the point
+             *                                 of intersection (facing to the 
+             *                                 side of the ray origin).
+             * @param[out] dist If ray and triangle intersect, dist will 
+             *                  contain the distance from the ray origin to the
+             *                  intersection point.
+             */
+            static inline bool rayTriangle(
+                const Ray& ray,
+                const Triangle& triangle,
+                Vec3& intersection_point,
+                Vec3& intersection_normal,
+                double& dist);
+            /** @todo documentation */
+            static inline bool lineTriangle(
+                const Vec3& line_start,
+                const Vec3& line_end,
+                const Triangle& triangle,
+                Vec3& intersection_point,
+                Vec3& intersection_normal,
+                double& dist);
+            /** @todo documentation */
+            static inline bool raySphere(
+                const Ray& ray,
+                const Vec3& sphere_center,
+                const double sphere_radius,
+                double& min_dist,
+                double& max_dist);
+            /** @todo documentation */
+            static inline bool raySphere(
+                const Ray& ray,
+                const Vec3& sphere_center,
+                const double sphere_radius,
+                Vec3& min_intersection_point,
+                Vec3& min_intersection_normal,
+                double& min_dist,
+                Vec3& max_intersection_point,
+                Vec3& max_intersection_normal,
+                double& max_dist);
+            /** @todo documentation */
+            static inline bool raySphereOnlyMin(
+                const Ray& ray,
+                const Vec3& sphere_center,
+                const double sphere_radius,
+                Vec3& intersection_point,
+                Vec3& intersection_normal,
+                double& dist);
+            /** @todo documentation */
+            static inline bool raySphereOnlyMax(
+                const Ray& ray,
+                const Vec3& sphere_center,
+                const double sphere_radius,
+                Vec3& intersection_point,
+                Vec3& intersection_normal,
+                double& dist);
+        };
+    }
+}
+
+#endif
