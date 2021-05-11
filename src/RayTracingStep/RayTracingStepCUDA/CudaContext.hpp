7119859fd47d43dbf7822beff1263eb15d023442
Alexander Gerwing <a_gerw03@uni-muenster.de>
2021-05-11 15:37:48 +0200

    Add cuda context holding cuda data

---
+++ src/RayTracingStep/RayTracingStepCUDA/CudaContext.hpp
@@ -0,0 +1,57 @@
+#ifndef WWU_LDPLAB_RTSCUDA_CUDA_CONTEXT_HPP
+#define WWU_LDPLAB_RTSCUDA_CUDA_CONTEXT_HPP
+#ifdef LDPLAB_BUILD_OPTION_ENABLE_RTSCUDA
+
+#include <LDPLAB/Geometry.hpp>
+
+#include "CudaResource.hpp"
+#include "Data.hpp"
+
+namespace ldplab
+{
+    namespace rtscuda
+    {
+        /** @brief Holds any resources acquired for the use of CUDA. */
+        struct CudaContext
+        {
+            /** @brief Ray buffer storage resources. */
+            struct
+            {
+                /** @brief 2D array holding index arrays per buffer. */
+                CudaPitchedPtr<uint32_t> index_buffers;
+                /** @brief 2D array of origin vector arrays per buffers. */
+                CudaPitchedPtr<Vec3> origin_buffers;
+                /** @brief 2D array of direction vector arrays per buffer. */
+                CudaPitchedPtr<Vec3> direction_buffers;
+                /** @brief 2D array of intensity arrays per buffer. */
+                CudaPitchedPtr<double> intensity_buffers;
+                /** 
+                 * @brief 2D array of minimum bounding volume distance arrays
+                 *        per buffer.
+                 */
+                CudaPitchedPtr<double> min_bounding_volume_distance_buffers;
+            } RayBufferData;
+            /** @brief Intersection buffer storage resources. */
+            struct
+            {
+                /** @brief Array of particle indices. */
+                CudaLinearArray<uint32_t> indices;
+                /** @brief Array of intersection points. */
+                CudaLinearArray<Vec3> intersection_points;
+                /** @brief Array of intersection normals. */
+                CudaLinearArray<Vec3> intersection_normal;
+            } IntersectionBufferData;
+            /** @brief Output buffer storage resources. */
+            struct
+            {
+                /** @brief Array containing force vectors per particle. */
+                CudaLinearArray<Vec3> force_per_particle;
+                /** @brief Array containing torque vectors per particle. */
+                CudaLinearArray<Vec3> torque_per_particle;
+            } OutputBufferData;
+        };
+    }
+}
+
+#endif // LDPLAB_BUILD_OPTION_ENABLE_RTSCUDA
+#endif // WWU_LDPLAB_RTSCUDA_CUDA_CONTEXT_HPP
